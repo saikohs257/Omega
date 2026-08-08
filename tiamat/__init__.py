2e85ac6be81ad0c494e76bcac1de13e11ead36be
saikohs257 <biffholderman@gmail.com>
2026-08-08 11:20:40 -0400

    TIAMAT V3.1 export probability and target contracts

--- tiamat/__init__.py
+++ tiamat/__init__.py
@@ -6,21 +6,12 @@ from tiamat.holdout import HOLDOUT_EXPERIMENT_VERSION, HoldoutEvaluation, Holdou
 from tiamat.identification_registry import CANONICAL_THRESHOLDS, MODEL_REGISTRY, ModelSpec, model, registry_fingerprint
 from tiamat.identification_runner import CandidateTrial, IDENTIFICATION_RUNNER_VERSION, IdentificationRunner, TournamentReport
 from tiamat.locked_evaluator import LockedModelEvaluator
-from tiamat.metric_contract import LABEL_PROVENANCE_VERSION, PROBABILITY_CONTRACT_VERSION, LabelProvenance, ProbabilityContract, validate_probability_rows
+from tiamat.metric_contract import LABEL_PROVENANCE_VERSION, PROBABILITY_CONTRACT_VERSION, STATE_SPACE, LabelProvenance, ProbabilityContract, ProbabilityPredictor, TargetProvenance, validate_probability_output, validate_probability_rows
 from tiamat.modes import TiamatMode
 from tiamat.replay import replay
 from tiamat.state import TiamatState
 from tiamat.telemetry import CANONICAL_CONTROL_AXES, TelemetryAdapter, TelemetryRow, axes_for_model
 from tiamat.transition import transition
 
-__all__ = [
-    "CandidateTrial", "CANONICAL_CONTROL_AXES", "CANONICAL_THRESHOLDS", "DEFAULT_METRIC_WEIGHTS", "Decision",
-    "ExperimentManifest", "FeatureDeclaration", "GuardResult", "HOLDOUT_EXPERIMENT_VERSION", "HoldoutEvaluation",
-    "HoldoutExperiment", "HoldoutSplit", "IDENTIFICATION_RUNNER_VERSION", "IdentificationRunner", "LABEL_PROVENANCE_VERSION",
-    "LockedModelEvaluator", "MODEL_REGISTRY", "ModelSpec", "PROBABILITY_CONTRACT_VERSION", "ProbabilityContract",
-    "LabelProvenance", "TEMPORAL_CAUSAL_GATE_VERSION", "TelemetryAdapter", "TelemetryRow", "TemporalCausalGate",
-    "TOURNAMENT_CONFIG_VERSION", "TiamatEngine", "TiamatMode", "TiamatState", "TournamentConfig", "TournamentReport",
-    "axes_for_model", "canonical_hash", "corpus_fingerprint", "evaluate_guards", "implementation_fingerprint", "model",
-    "provenance_fingerprint", "registry_fingerprint", "replay", "transition", "validate_probability_rows",
-]
+__all__ = ["CandidateTrial", "CANONICAL_CONTROL_AXES", "CANONICAL_THRESHOLDS", "DEFAULT_METRIC_WEIGHTS", "Decision", "ExperimentManifest", "FeatureDeclaration", "GuardResult", "HOLDOUT_EXPERIMENT_VERSION", "HoldoutEvaluation", "HoldoutExperiment", "HoldoutSplit", "IDENTIFICATION_RUNNER_VERSION", "IdentificationRunner", "LABEL_PROVENANCE_VERSION", "LockedModelEvaluator", "MODEL_REGISTRY", "ModelSpec", "PROBABILITY_CONTRACT_VERSION", "ProbabilityContract", "ProbabilityPredictor", "STATE_SPACE", "TargetProvenance", "LabelProvenance", "TEMPORAL_CAUSAL_GATE_VERSION", "TelemetryAdapter", "TelemetryRow", "TemporalCausalGate", "TOURNAMENT_CONFIG_VERSION", "TiamatEngine", "TiamatMode", "TiamatState", "TournamentConfig", "TournamentReport", "axes_for_model", "canonical_hash", "corpus_fingerprint", "evaluate_guards", "implementation_fingerprint", "model", "provenance_fingerprint", "registry_fingerprint", "replay", "transition", "validate_probability_output", "validate_probability_rows"]
 TIAMAT_CONFIG_VERSION = TOURNAMENT_CONFIG_VERSION
